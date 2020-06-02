3d041d83c1076485010d23a2bb6c9caef15a9902
LeMinhChinh <LeMinhChinh@users.noreply.github.com>
2020-06-02 22:58:53 +0700

    room continue

--- resources/views/user/room.blade.php
+++ resources/views/user/room.blade.php
@@ -15,7 +15,7 @@
                    <div class="row">
                         @foreach ($inforRoom as $key =>  $value)
                             <div class="col-6">
-                                <div class="room-item" data-toggle="modal" data-target="#detailRoom">
+                                <div class="room-item" data-toggle="modal" data-target="#detailRoom-{{ $key }}">
                                     <div class="room-item-img">
                                         @foreach ($images as $k => $v)
                                             @if($key == $k)
@@ -38,9 +38,9 @@
                                                 @endif
                                             </div>
                                             <div class="rp-item-featured">
-                                                <p class="rp-item-people">Dành cho : <span>{{ $value['adult'] }} người lớn <i class="fas fa-user-friends"></i> và {{ $value['child'] }} trẻ con <i class="fas fa-baby"></i></p>
+                                                <p class="rp-item-people">Dành cho : @if($value['adult'] > 0 && $value['child'] == 0)<span>{{ $value['adult'] }} người lớn <i class="fas fa-user-friends"></i>@endif @if($value['child'] > 0 && $value['adult'] == 0) {{ $value['child'] }} trẻ con <i class="fas fa-baby"></i>@endif @if($value['adult'] > 0 && $value['child'] > 0)<span>{{ $value['adult'] }} người lớn <i class="fas fa-user-friends"></i> và {{ $value['child'] }} trẻ con <i class="fas fa-baby"></i> @endif</p>
                                                 @if($value['acreage'] > 0)<p><i class="fa fa-home"></i>Diện tích phòng :  {{ $value['acreage'] }}m&#178;</p>@endif
-                                                <p><i class="fa fa-bed"></i>{{ $value['quantity_bed'] }} {{ $value['name_bed'] }}</p>
+                                                @if($value['quantity_bed'] > 0)<p><i class="fa fa-bed"></i>{{ $value['quantity_bed'] }} @if($value['type_bed'] > 0) {{ $value['name_bed'] }} @endif @if($value['description_bed'] !== null) {{ $value['description_bed'] }} @endif</p>@endif
                                                 @if($value['smoke'] == 0)
                                                     <p><i class="fas fa-smoking"></i>Không hút thuốc</p>
                                                 @else
@@ -55,11 +55,11 @@
                                     </div>
                                 </div>
                             </div>
-                            <div class="modal fade" id="detailRoom" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal fade" id="detailRoom-{{ $key }}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
                                 <div class="modal-dialog popup-detail" role="document">
                                 <div class="modal-content content-detail">
                                     <div class="modal-header">
-                                    <h5 class="modal-title" id="exampleModalLabel"><p class="rp-item-name-room"><small>Khách sạn </small> Fusion Suites Saigon - <small>Phòng </small>202A</p></h5>
+                                    <h5 class="modal-title" id="exampleModalLabel"><p class="rp-item-name-room"><small>Khách sạn </small> Fusion Suites Saigon - <small>Phòng </small>{{ $value['name'] }}</p></h5>
                                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                         <span aria-hidden="true">&times;</span>
                                     </button>
@@ -68,32 +68,38 @@
                                         <div class="row">
                                             <div class="col-3">
                                                 <div class="rp-item-infor">
-                                                    <div class="rp-item-feedback">
+                                                    {{-- <div class="rp-item-feedback">
                                                         <p class="rp-item-title-index">Đánh giá của khách hàng : </p>
                                                         <a href="">Tuyệt vời 8,9 - 245 đánh giá</a>
-                                                    </div>
+                                                    </div> --}}
                                                     <div class="rp-item-featured">
                                                         <div class="rp-item-feedback">
-                                                            <p class="rp-item-title-index">Thông tin chung : </p>
-                                                            <p class="rp-item-people">Phòng ngủ dành cho : <span>5 người <i class="fas fa-users"></i> <i class="fas fa-user-friends" style="display: none;"></i><i class="fas fa-user" style="display: none;"></i></span></p>
-                                                            <p><i class="fa fa-home"></i>Diện tích phòng :  50m&#178;</p>
-                                                            <p><i class="fa fa-bed"></i>1 giường đơn</p>
-                                                            <p><i class="fas fa-smoking"></i>Không hút thuốc</p>
+                                                            <p class="rp-item-people">Phòng ngủ dành cho : @if($value['adult'] > 0 && $value['child'] == 0)<span>{{ $value['adult'] }} người lớn <i class="fas fa-user-friends"></i>@endif @if($value['child'] > 0 && $value['adult'] == 0) {{ $value['child'] }} trẻ con <i class="fas fa-baby"></i>@endif @if($value['adult'] > 0 && $value['child'] > 0)<span>{{ $value['adult'] }} người lớn <i class="fas fa-user-friends"></i> và {{ $value['child'] }} trẻ con <i class="fas fa-baby"></i> @endif</p>
+                                                            @if($value['acreage'] > 0)<p><i class="fa fa-home"></i>Diện tích phòng :  {{ $value['acreage'] }}m&#178;</p>@endif
+                                                            @if($value['quantity_bed'] > 0)<p><i class="fa fa-bed"></i>{{ $value['quantity_bed'] }} @if($value['type_bed'] > 0) {{ $value['name_bed'] }} @endif @if($value['description_bed'] !== null) {{ $value['description_bed'] }} @endif</p>@endif
+                                                            @if($value['smoke'] == 0)
+                                                                <p><i class="fas fa-smoking"></i>Không hút thuốc</p>
+                                                            @else
+                                                                <p><i class="fas fa-smoking"></i>Có hút thuốc</p>
+                                                            @endif
                                                         </div>
-                                                        <div class="rp-item-feedback">
+                                                        @if($value['wifi'] > 0 && $value['phone'] > 0 && $value['television'] > 0)<div class="rp-item-feedback">
                                                             <p class="rp-item-title-index">Giải trí : </p>
-                                                            <p><i class="fas fa-wifi"></i>Wifi miễn phí tất cả các phòng</p>
-                                                            <p><i class="fas fa-phone-alt"></i>Điện thoại</p>
-                                                            <p><i class="fas fa-tv"></i>Televison</p>
-                                                        </div>
-                                                        <div class="rp-item-feedback">
+                                                            @if($value['wifi'] > 0)<p><i class="fas fa-wifi"></i>Wifi miễn phí tất cả các phòng</p>@endif
+                                                            @if($value['phone'] > 0)<p><i class="fas fa-phone-alt"></i>Điện thoại</p>@endif
+                                                            @if($value['television'] > 0)<p><i class="fas fa-tv"></i>Televison</p>@endif
+                                                        </div>@endif
+                                                        @if($value['air_conditioning'] > 0)<div class="rp-item-feedback">
                                                             <p class="rp-item-title-index">Tiện nghi : </p>
                                                             <p><img src="https://img.icons8.com/pastel-glyph/64/000000/air-conditioner--v2.png"/>Điều hòa</p>
-                                                        </div>
+                                                        </div>@endif
                                                         <div class="rp-item-feedback">
-                                                            <p class="rp-item-title-index">Giá : </p>
-                                                            <p class="rp-item-price"><i class="fas fa-dollar-sign"></i> Giá gốc : <span>1.245.547&#8363;</span></p><p class="rp-item-discount">Giảm giá đến <span>5%</span></p>
-                                                            <p class="rp-item-promo-price">Nay chỉ còn : <span>985.542&#8363;</span></p>
+                                                            @if($value['discount'] > 0)
+                                                                <p class="rp-item-price rp-price-discount"><i class="fas fa-dollar-sign"></i> Giá gốc : <span>{{ number_format($value['price'] ,0 ,'.' ,'.').'' }}&#8363;</span></p><p class="rp-item-discount">Giảm giá đến <span>{{ $value['discount'] }}%</span></p>
+                                                                <p class="rp-item-promo-price">Nay chỉ còn : <span>{{ number_format($value['price'] - ($value['price']*$value['discount']/100) ,0 ,'.' ,'.').'' }}&#8363;</span></p>
+                                                            @else
+                                                                <p class="rp-item-price"><i class="fas fa-dollar-sign"></i> Giá gốc : <span>{{ number_format($value['price'] ,0 ,'.' ,'.').'' }}&#8363;</span></p>
+                                                            @endif
                                                         </div>
                                                     </div>
                                                 </div>
@@ -102,7 +108,7 @@
                                                 <div class="rp-detail-img">
                                                     <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
                                                         <ol class="carousel-indicators">
-                                                            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active">
+                                                            {{-- <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active">
                                                                 <div class="img-active">
                                                                     <img src="img/summer-trip-hồ-cốc-1.jpg" alt="" >
                                                                 </div>
@@ -116,10 +122,21 @@
                                                                 <div class="img-active">
                                                                     <img src="img/summer-trip-hồ-cốc-1.jpg" alt="" >
                                                                 </div>
-                                                            </li>
+                                                            </li> --}}
+                                                            @foreach ($images as $index => $img)
+                                                                @if($index == $key)
+                                                                    @foreach($img as $i => $item)
+                                                                        <li data-target="#carouselExampleIndicators" data-slide-to="{{ $i }}" class="active">
+                                                                            <div class="img-active">
+                                                                                <img src="{{ URL::to('/') }}/user/uploads/room/{{ $item }}" alt="" >
+                                                                            </div>
+                                                                        </li>
+                                                                    @endforeach
+                                                                @endif
+                                                            @endforeach
                                                         </ol>
                                                         <div class="carousel-inner">
-                                                            <div class="carousel-item active">
+                                                            {{-- <div class="carousel-item active">
                                                                 <img class="d-block w-100" src="img/summer-trip-hồ-cốc-1.jpg" alt="First slide">
                                                             </div>
                                                             <div class="carousel-item">
@@ -127,7 +144,33 @@
                                                             </div>
                                                             <div class="carousel-item">
                                                                 <img class="d-block w-100" src="img/summer-trip-hồ-cốc-1.jpg" alt="Third slide">
-                                                            </div>
+                                                            </div> --}}
+{{--
+                                                            <div class="carousel-item active">
+                                                                <img class="d-block w-100" src="{{ URL::to('/') }}/user/uploads/resting_place/{{ $image[0] }}" >
+                                                            </div> --}}
+                                                            @foreach ($images as $index => $img)
+                                                                @if($index == $key)
+                                                                    <div class="carousel-item active">
+                                                                        <img class="d-block w-100" src="{{ URL::to('/') }}/user/uploads/room/{{ $img[0] }}" >
+                                                                    </div>
+                                                                    @foreach($img as $i => $item)
+
+                                                                        @if($i > 0)
+                                                                            <div class="carousel-item">
+                                                                                <img class="d-block w-100" src="{{ URL::to('/') }}/user/uploads/room/{{ $item }}" alt="{{ $i }} slide">
+                                                                            </div>
+                                                                        @endif
+                                                                    @endforeach
+                                                                @endif
+                                                            @endforeach
+                                                            {{-- @foreach ($image as $key => $img)
+                                                                @if($key > 0)
+                                                                    <div class="carousel-item">
+                                                                        <img class="d-block w-100" src="{{ URL::to('/') }}/user/uploads/resting_place/{{ $img }}" alt="{{ $key }} slide">
+                                                                    </div>
+                                                                @endif
+                                                            @endforeach --}}
                                                         </div>
                                                         <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
                                                             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
